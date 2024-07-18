c90532bb8b5639ff1d303d98cba27cb06eed4766
Diego Armando <dgox16@gmail.com>
2024-07-18 13:05:23 -0600

    Se dividio la pagina de visualizar detalles a varios componentes

---
+++ src/routes/polizas/visualizar/[id]/PrincipalVerPoliza.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+    import MainTitle from "$lib/components/layout/MainTitle.svelte";
+    import * as Card from "$lib/components/ui/card/index.js";
+    import { format } from "date-fns";
+    import type { Poliza } from "$lib/modelos/polizas/polizaBasica";
+    export let poliza: Poliza;
+    export let poliza_egreso;
+</script>
+
+<MainTitle
+    titulo="Visualización de poliza"
+    subtitulo="Puedes revisar todos los detalle de la poliza"
+/>
+<div class="grid grid-cols-3 gap-4">
+    <span><span class="font-bold">ID: </span>{poliza.id_poliza}</span>
+    <span class="col-span-2"
+        ><span class="font-bold">Concepto: </span>{poliza.concepto}</span
+    >
+    <span><span class="font-bold">Tipo: </span>{poliza.tipo}</span>
+    <span
+        ><span class="font-bold">Fecha: </span>{format(
+            new Date(poliza.fecha_poliza),
+            "dd/MM/yyyy HH:mm:ss",
+        )}</span
+    >
+    <span><span class="font-bold">Sucursal: </span>{poliza.sucursal}</span>
+    <span><span class="font-bold">Fuente: </span>{poliza.fuente}</span>
+    <span><span class="font-bold">Aplicación: </span>{poliza.aplicacion}</span>
+    <span
+        ><span class="font-bold"
+            >ID del usuario:
+        </span>{poliza.usuario_elabora}</span
+    >
+</div>
+
+{#if poliza.tipo == "Egreso"}
+    <Card.Root class="mt-7">
+        <Card.Header>
+            <Card.Title class="text-2xl">Datos de egreso</Card.Title>
+        </Card.Header>
+        <Card.Content>
+            <div class="grid grid-cols-3 gap-4">
+                <span
+                    ><span class="font-bold"
+                        >Beneficiario:
+                    </span>{poliza_egreso.beneficiario}</span
+                >
+                <span
+                    ><span class="font-bold"
+                        >Banco:
+                    </span>{poliza_egreso.banco}</span
+                >
+                <span
+                    ><span class="font-bold"
+                        >Cheque:
+                    </span>{poliza_egreso.cheque}</span
+                >
+            </div>
+        </Card.Content>
+    </Card.Root>
+{/if}
---
+++ src/routes/polizas/visualizar/[id]/TablaDetallesVerPoliza.svelte
@@ -0,0 +1,83 @@
+<script lang="ts">
+    import * as Table from "$lib/components/ui/table/index.js";
+    import * as DropdownMenu from "$lib/components/ui/dropdown-menu/index.js";
+    import { toast } from "svelte-sonner";
+    import { Button } from "$lib/components/ui/button/index.js";
+    import { DotsHorizontal } from "svelte-radix";
+
+    export let detalles_poliza;
+
+    const eliminarDetallePoliza = async (id: Number) => {
+        const respuesta = await fetch(
+            `http://localhost:8000/api/poliza/detalles/eliminar/${id}`,
+            {
+                method: "DELETE",
+            },
+        );
+        const resultado = await respuesta.json();
+        if (!resultado.estado) {
+            toast.error("Error al eliminar el detalle.");
+        } else {
+            toast.info("Detalle eliminado correctamente.");
+            detalles_poliza = detalles_poliza.filter(
+                (d) => d.id_detalle_poliza !== id,
+            );
+        }
+    };
+</script>
+
+{#if detalles_poliza == undefined}
+    <div class="flex justify-center items-center my-6">
+        <h1 class="text-lg font-semibold uppercase">No hay detalles</h1>
+    </div>
+{:else}
+    <Table.Root>
+        <Table.Header>
+            <Table.Row>
+                <Table.Head class="w-[100px]">ID</Table.Head>
+                <Table.Head>Concepto</Table.Head>
+                <Table.Head>Cuenta</Table.Head>
+                <Table.Head>Iva</Table.Head>
+                <Table.Head>Cargo</Table.Head>
+                <Table.Head>Abono</Table.Head>
+                <Table.Head class="w-[10px]"></Table.Head>
+            </Table.Row>
+        </Table.Header>
+        <Table.Body>
+            {#each detalles_poliza as detalle, i (i)}
+                <Table.Row>
+                    <Table.Cell class="font-medium"
+                        >{detalle.id_detalle_poliza}</Table.Cell
+                    >
+                    <Table.Cell>{detalle.concepto}</Table.Cell>
+                    <Table.Cell>{detalle.cuenta}</Table.Cell>
+                    <Table.Cell>{detalle.iva}</Table.Cell>
+                    <Table.Cell>{detalle.cargo}</Table.Cell>
+                    <Table.Cell>{detalle.abono}</Table.Cell>
+                    <Table.Cell>
+                        <DropdownMenu.Root>
+                            <DropdownMenu.Trigger asChild let:builder>
+                                <Button builders={[builder]} variant="ghost"
+                                    ><DotsHorizontal class="size-5" /></Button
+                                >
+                            </DropdownMenu.Trigger>
+                            <DropdownMenu.Content class="w-40">
+                                <DropdownMenu.Group>
+                                    <DropdownMenu.Item
+                                        on:click={() => {
+                                            eliminarDetallePoliza(
+                                                detalle.id_detalle_poliza,
+                                            );
+                                        }}
+                                        rel="external"
+                                        >Eliminar</DropdownMenu.Item
+                                    >
+                                </DropdownMenu.Group>
+                            </DropdownMenu.Content>
+                        </DropdownMenu.Root>
+                    </Table.Cell>
+                </Table.Row>
+            {/each}
+        </Table.Body>
+    </Table.Root>
+{/if}
