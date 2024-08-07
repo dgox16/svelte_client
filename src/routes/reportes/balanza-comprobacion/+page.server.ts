269904dcfd22bf6ae9e3b01f45b5058c261ad945
Diego Armando <dgox16@gmail.com>
2024-08-06 22:37:13 -0600

    Se termino la parte de la pagina para la nueva implementacion de balanza de comprobacion

--- src/routes/reportes/balanza-comprobacion/+page.server.ts
+++ src/routes/reportes/balanza-comprobacion/+page.server.ts
@@ -2,19 +2,22 @@ import type { PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async () => {
 	const hoy = new Date();
-	const fechaFormateada = hoy.toISOString().split("T")[0];
+	const unaSemanaAtras = new Date(hoy);
+	unaSemanaAtras.setDate(hoy.getDate() - 15);
+	const fechaFormateadaAntes = unaSemanaAtras.toISOString().split("T")[0];
+	const fechaFormateadaHoy = hoy.toISOString().split("T")[0];
 
 	const respuesta = await fetch(
-		`http://localhost:8000/api/poliza/balanza_comprobacion?fecha=${fechaFormateada}`,
+		`http://localhost:8000/api/balanza_comprobacion?fecha_inicial=${fechaFormateadaAntes}&fecha_final=${fechaFormateadaHoy}`,
 	);
-
 	const resultado = await respuesta.json();
 
 	let cuentas = resultado.datos.balanza;
 	let total = resultado.datos.total;
 
 	return {
-		fechaActual: fechaFormateada,
+		fechaInicial: fechaFormateadaAntes,
+		fechaFinal: fechaFormateadaHoy,
 		cuentas,
 		total,
 	};
--- src/routes/reportes/balanza-comprobacion/+page.svelte
+++ src/routes/reportes/balanza-comprobacion/+page.svelte
@@ -15,6 +15,7 @@
     } from "@internationalized/date";
     import { CalendarIcon } from "lucide-svelte";
     import type { PageData } from "./$types";
+    import Label from "$lib/components/ui/label/label.svelte";
 
     export let data: PageData;
     let cuentas = data.cuentas;
@@ -24,8 +25,10 @@
         dateStyle: "long",
     });
 
-    let fechaMostrar: DateValue | undefined = undefined;
-    let fechaApi = data.fechaActual;
+    let fechaMostrarInicial: DateValue | undefined;
+    let fechaMostrarFinal: DateValue | undefined;
+    let fechaApiInicial = data.fechaInicial;
+    let fechaApiFinal = data.fechaFinal;
 </script>
 
 <div class="flex flex-row justify-between items-center">
@@ -53,56 +56,119 @@
 
 <Card.Root class="mt-7">
     <Card.Header>
-        <Popover.Root>
-            <Popover.Trigger asChild let:builder>
-                <Button
-                    variant="outline"
-                    class={cn(
-                        "w-[280px] justify-start text-left font-normal",
-                        !fechaMostrar && "text-muted-foreground",
-                    )}
-                    builders={[builder]}
-                >
-                    <CalendarIcon class="mr-2 h-4 w-4" />
-                    {fechaMostrar
-                        ? df.format(fechaMostrar.toDate(getLocalTimeZone()))
-                        : "Selecciona una fecha"}
-                </Button>
-            </Popover.Trigger>
-            <Popover.Content class="w-auto p-0">
-                <Calendar
-                    locale="es"
-                    bind:fechaMostrar
-                    initialFocus
-                    onValueChange={async (v) => {
-                        if (v) {
-                            fechaMostrar = v;
-                            fechaApi = v
-                                .toDate(getLocalTimeZone())
-                                .toLocaleDateString("sv-SE");
+        <div class="flex flex-row">
+            <div class="flex flex-row items-center">
+                <Label class="mr-5 font-medium">Fecha inicial:</Label>
+                <Popover.Root>
+                    <Popover.Trigger asChild let:builder>
+                        <Button
+                            variant="outline"
+                            class={cn(
+                                "w-[280px] justify-start text-left font-normal",
+                                !fechaMostrarInicial && "text-muted-foreground",
+                            )}
+                            builders={[builder]}
+                        >
+                            <CalendarIcon class="mr-2 h-4 w-4" />
+                            {fechaMostrarInicial
+                                ? df.format(
+                                      fechaMostrarInicial.toDate(
+                                          getLocalTimeZone(),
+                                      ),
+                                  )
+                                : "Selecciona la fecha inicial"}
+                        </Button>
+                    </Popover.Trigger>
+                    <Popover.Content class="w-auto p-0">
+                        <Calendar
+                            locale="es"
+                            value={fechaMostrarInicial}
+                            maxValue={fechaMostrarFinal}
+                            initialFocus
+                            onValueChange={async (v) => {
+                                if (v) {
+                                    fechaMostrarInicial = v;
+                                    fechaApiInicial = v
+                                        .toDate(getLocalTimeZone())
+                                        .toLocaleDateString("sv-SE");
 
-                            const respuesta = await fetch(
-                                `http://localhost:8000/api/poliza/balanza_comprobacion?fecha=${fechaApi}`,
-                            );
-                            const resultado = await respuesta.json();
-                            cuentas = resultado.datos.balanza;
-                            total = resultado.datos.total;
-                        } else {
-                            fechaMostrar = undefined;
-                        }
-                    }}
-                />
-            </Popover.Content>
-        </Popover.Root>
+                                    const respuesta = await fetch(
+                                        `http://localhost:8000/api/balanza_comprobacion?fecha_inicial=${fechaApiInicial}&fecha_final=${fechaApiFinal}`,
+                                    );
+                                    const resultado = await respuesta.json();
+                                    cuentas = resultado.datos.balanza;
+                                    total = resultado.datos.total;
+                                } else {
+                                    fechaMostrarInicial = undefined;
+                                }
+                            }}
+                        />
+                    </Popover.Content>
+                </Popover.Root>
+            </div>
+            <div class="flex flex-row items-center">
+                <Label class="ml-7 mr-5 font-medium">Fecha final:</Label>
+                <Popover.Root>
+                    <Popover.Trigger asChild let:builder>
+                        <Button
+                            variant="outline"
+                            class={cn(
+                                "w-[280px] justify-start text-left font-normal",
+                                !fechaMostrarFinal && "text-muted-foreground",
+                            )}
+                            builders={[builder]}
+                        >
+                            <CalendarIcon class="mr-2 h-4 w-4" />
+                            {fechaMostrarFinal
+                                ? df.format(
+                                      fechaMostrarFinal.toDate(
+                                          getLocalTimeZone(),
+                                      ),
+                                  )
+                                : "Selecciona la fecha final"}
+                        </Button>
+                    </Popover.Trigger>
+                    <Popover.Content class="w-auto p-0">
+                        <Calendar
+                            locale="es"
+                            value={fechaMostrarFinal}
+                            minValue={fechaMostrarInicial}
+                            initialFocus
+                            onValueChange={async (v) => {
+                                if (v) {
+                                    fechaMostrarFinal = v;
+                                    fechaApiFinal = v
+                                        .toDate(getLocalTimeZone())
+                                        .toLocaleDateString("sv-SE");
+
+                                    const respuesta = await fetch(
+                                        `http://localhost:8000/api/balanza_comprobacion?fecha_inicial=${fechaApiInicial}&fecha_final=${fechaApiFinal}`,
+                                    );
+                                    const resultado = await respuesta.json();
+                                    cuentas = resultado.datos.balanza;
+                                    total = resultado.datos.total;
+                                } else {
+                                    fechaMostrarInicial = undefined;
+                                }
+                            }}
+                        />
+                    </Popover.Content>
+                </Popover.Root>
+            </div>
+        </div>
     </Card.Header>
     <Card.Content>
         <Table.Root>
             <Table.Header>
                 <Table.Row>
                     <Table.Head class="w-[100px]">ID</Table.Head>
+                    <Table.Head class="w-[100px]">Nombre</Table.Head>
                     <Table.Head>Deudora Anterior</Table.Head>
-                    <Table.Head>Abono</Table.Head>
+                    <Table.Head>Acreedora Anterior</Table.Head>
                     <Table.Head>Cargo</Table.Head>
+                    <Table.Head>Abono</Table.Head>
+                    <Table.Head>Saldo Deudor</Table.Head>
+                    <Table.Head>Saldo Acreedor</Table.Head>
                 </Table.Row>
             </Table.Header>
             <Table.Body>
@@ -111,9 +177,13 @@
                         <Table.Cell class="font-bold"
                             >{cuenta.cuenta}</Table.Cell
                         >
-                        <Table.Cell>{cuenta.total}</Table.Cell>
-                        <Table.Cell>{cuenta.total_abono}</Table.Cell>
-                        <Table.Cell>{cuenta.total_cargo}</Table.Cell>
+                        <Table.Cell>{cuenta.nombre}</Table.Cell>
+                        <Table.Cell>{cuenta.deudora_anterior}</Table.Cell>
+                        <Table.Cell>{cuenta.acreedora_anterior}</Table.Cell>
+                        <Table.Cell>{cuenta.cargo}</Table.Cell>
+                        <Table.Cell>{cuenta.abono}</Table.Cell>
+                        <Table.Cell>{cuenta.saldo_deudor}</Table.Cell>
+                        <Table.Cell>{cuenta.saldo_acreedor}</Table.Cell>
                     </Table.Row>
                 {/each}
             </Table.Body>
